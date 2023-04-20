c782e362998e842b69136dbb4813e1b104323965
Ben Wiens <bwiens36@gmail.com>
2023-04-20 17:18:37 -0500

    first commit

---
+++ R/betterKUenm.R
@@ -0,0 +1,21 @@
+#' betterKUenm: a better version of kuenm
+#'
+#' @description
+#' This is the description for the package. It is very informative.
+#'
+#'
+#' @details
+#' Lots and lots of details here.
+#'
+#'
+#' @section Main functions in biosurvey:
+#' \code{\link{ext_back}}, \code{\link{fitGLM}},
+#' \code{\link{occ_prep}}, \code{\link{projectionGLM}}
+#'
+#' @section Other functions (important helpers):
+#'
+#' @section Data included:
+#'
+#' @docType package
+#' @name betterKUenm
+NULL
